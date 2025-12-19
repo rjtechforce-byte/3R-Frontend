060cc63b006241fe90f77207a269ec4d70bdf8f1
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-19 12:09:58 +0530

    Update thumbnail URL for school data cards

--- src/pages/YourSchool.jsx
+++ src/pages/YourSchool.jsx
@@ -33,7 +33,7 @@ const YourSchool = ({ auth, showAlert }) => {
               <a href={"/product/" + auth._id + "/helpedStudent"} className='bg-green-800 cursor-pointer text-white px-15 xl:mt-27 py-2 mt-7 border-2 shadow-xl border-white-800  inline-flex gap-2 font-semibold text-4xl font-serif items-center transition-all duration-700 rounded-[9px] group hover:text-green-800 hover:bg-white'>Add Helped Student</a>
             </div>
             <div className="flex flex-wrap justify-center">
-              {schoolDataList2 || [].map((schoolData, index) => <Cards key={schoolData._id || index} title={schoolData.title} _id={schoolData._id} availability={schoolData.availability ? "Available" : "Unavailable"} thumbnail={`http://localhost:3000/${schoolData.thumbnail}`} />)}
+              {schoolDataList2 || [].map((schoolData, index) => <Cards key={schoolData._id || index} title={schoolData.title} _id={schoolData._id} availability={schoolData.availability ? "Available" : "Unavailable"} thumbnail={`https://rrr-frontend.onrender.com/${schoolData.thumbnail}`} />)}
             </div>
           </div>
         ) : (
