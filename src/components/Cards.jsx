3fa300fb631ff71631a521e826fb36d42e724080
rjtechforce-byte <rjtechforce@gmail.com>
2025-12-20 14:06:47 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,48 +1,25 @@
-import React, { useContext, useState, useEffect } from "react";
-import SelectSchool from "../components/SelectSchool";
-import Slidebar from "../components/Slidebar";
-import { AllProductsData } from "../context/AllProducts";
-import SchoolLoginForm from "../components/form/SchoolLoginForm";
-import { getSchoolProducts } from "../components/form/api"; 
-import Cards from "../components/Cards";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const YourSchool = ({ auth, showAlert }) => {
-  const [schoolDataList2, setSchoolDataList2] = useState([]);
-  const schoolDataList = useContext(AllProductsData);
 
-  console.log("School Data List:", schoolDataList);
-  useEffect(() => {
-    if (auth) {
-      getSchoolProducts(auth._id)
-        .then((products) => {
-          setSchoolDataList2(products);
-          console.log("Products for school:", products);
-        })
-    }
-  }, [auth]);
+const Cards = (props) => {
 
-  console.log("scholldata", schoolDataList2);
-
-  return (
-    <>
-      <Slidebar />
-      <div className="min-h-screen w-full">
-        {auth ? (
-          <div className="flex items-center flex-col gap-10">
-            <div className="flex justify-around min-w-full">
-              <a href="/uploadProducts" className="bg-white cursor-pointer text-green-800 px-15 xl:mt-27 py-2 mt-7 border-2 shadow-xl border-white-800  inline-flex gap-2 font-semibold text-4xl font-serif items-center transition-all duration-700 rounded-[9px] group hover:text-white hover:bg-green-800">Upload Products</a>
-            </div>
-            <div className="flex flex-wrap justify-center">
-              {schoolDataList2.map((schoolData, index) => <Cards key={schoolData._id || index} title={schoolData.title} _id={schoolData._id} availability={schoolData.availability ? "Available" : "Unavailable"} thumbnail={`https://rrr-backend-0wj5.onrender.com/${schoolData.thumbnail}`} />)}
-            </div>
-          </div>
-        ) : (
-          <SchoolLoginForm showAlert={showAlert} />
-        )}
-      </div>
-    </>
-  );
+    return (
+        <>
+        <div className="gap-5 sm:m-5 border border-gray-100 w-[350px] bg-white rounded-2xl shadow-md hover:shadow-xl hover:scale-110 m-5 transform  hover:-translate-y-0.5 font-serif transition-all duration-700 h-fit flex justify-center flex-col items-center">
+          
+            <img 
+              src={props.thumbnail} 
+              alt={props.schoolName} 
+              className="sm:p-5 w-full min-h-64 max-h-64 object-contain bg-gray-50 rounded-xl"
+            />
+            <p className="text-3xl font-serif text-center font-bold text-green-800">{props.title}</p> 
+            <h3 className="text-3xl font-serif text-center font-bold text-green-800">{props.availability}</h3>
+            <a href={"/productEdit/" + props._id} className='bg-green-800 justify-center cursor-pointer text-white px-8 py-2 m-2 shadow-xl inline-flex gap-2 font-semibold hover:border hover:border-green-900 text-center min-w-2xs text-2xl font-serif items-center transition-all duration-700 rounded-[9px] group hover:text-green-800 hover:bg-white'>Edit Product</a>
+            <a href={"/product/" + props._id + "/helpedStudent"} className='bg-green-800 text-center min-w-2xs cursor-pointer text-white px-8 py-2 m-2 mb-5 shadow-xl inline-flex hover:border hover:border-green-900 gap-2 font-semibold text-2xl font-serif items-center transition-all duration-700 rounded-[9px] group hover:text-green-800 hover:bg-white'>Add Helped Student</a>
+        </div>
+        </>
+    );
 };
 
-export default YourSchool;
-
+export default Cards;
