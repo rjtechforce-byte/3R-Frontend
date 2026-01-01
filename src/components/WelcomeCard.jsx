e75781b130a033caeeb99fa834f02dcbe7e0f5ed
rjtechforce-byte <rjtechforce@gmail.com>
2026-01-01 15:14:38 +0530

    Update WelcomeCard.jsx

--- src/components/WelcomeCard.jsx
+++ src/components/WelcomeCard.jsx
@@ -5,13 +5,13 @@ const WelcomeCard = (props) => {
     return (
         <div 
           
-          className='flex  flex-col shrink-0 w-[300px] h-[400px] p-6 bg-green-700 rounded-xl shadow-2xl items-center justify-center text-white gap-5'
+          className='flex transition-transform duration-500 hover:scale-105 flex-col shrink-0 w-[300px] h-[400px] p-6 bg-green-700 rounded-xl shadow-2xl items-center justify-center text-white gap-5'
         >
           
             <img 
               src={props.image} 
               alt={props.name} 
-              className="w-32 h-32 object-contain mb-4" 
+              className="w-full h-full object-contain mb-4" 
             />
             <p className="text-3xl font-serif text-center font-bold">{props.name}</p> 
         
